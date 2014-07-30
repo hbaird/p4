dbc725865e4858ab52f83abe81f5fd4289047563
Heather Baird <hbaird@fas.harvard.edu>
2014-07-30 18:51:37 -0400

    Production database file

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'crocodilians',
+            'username'  => 'hbaird',
+            'password'  => 'Harvard1der',
+        ),
+
+    ),
+
+);
