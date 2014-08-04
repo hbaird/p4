8c6ae318a6994e09b4d01636e12baec2a0caa652
Heather Baird <hbaird@fas.harvard.edu>
2014-08-03 22:11:00 -0400

    index, debug, user controllers

--- app/routes.php
+++ app/routes.php
@@ -11,97 +11,23 @@
 |
 */
 
-# Home page
-Route::get('/', function() {
-	return View::make('index');				
-});
+/*-------------------------------------------------------------------------------------------------
+// ! Index
+-------------------------------------------------------------------------------------------------*/
+Route::get('/', 'IndexController@getIndex');
 
 
+/*-------------------------------------------------------------------------------------------------
+// ! User
+Explicit Routing
+-------------------------------------------------------------------------------------------------*/
+# Note: the beforeFilter for 'guest' on getSignup and getLogin is handled in the Controller
+Route::get('/signup', 'UserController@getSignup'); 
+Route::get('/login', 'UserController@getLogin' );
+Route::post('/signup', ['before' => 'csrf', 'uses' => 'UserController@postSignup'] );
+Route::post('/login', ['before' => 'csrf', 'uses' => 'UserController@postLogin'] );
+Route::get('/logout', ['before' => 'auth', 'uses' => 'UserController@getLogout'] );
 
-/*****Debugging environments*******/
-
-//Get environmnet
-Route::get('/get-environment',function() {
-
-    echo "Environment: ".App::environment();
-
-});
-
-//Check error reporting
-Route::get('/trigger-error',function() {
-
-    # Class Foobar should not exist, so this should create an error
-    $foo = new Foobar;
-
-});
-
-Route::get('mysql-test', function() {
-
-    # Use the DB component to select all the databases
-    $results = DB::select('SHOW DATABASES;');
-
-    # If the "Pre" package is not installed, you should output using print_r instead
-    return Pre::render($results);
-
-});
-
-# /app/routes.php
-Route::get('/debug', function() {
-
-    echo '<pre>';
-
-    echo '<h1>environment.php</h1>';
-    $path   = base_path().'/environment.php';
-
-    try {
-        $contents = 'Contents: '.File::getRequire($path);
-        $exists = 'Yes';
-    }
-    catch (Exception $e) {
-        $exists = 'No. Defaulting to `production`';
-        $contents = '';
-    }
-
-    echo "Checking for: ".$path.'<br>';
-    echo 'Exists: '.$exists.'<br>';
-    echo $contents;
-    echo '<br>';
-
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
-
-    echo '<h1>Debugging?</h1>';
-    if(Config::get('app.debug')) echo "Yes"; else echo "No";
-
-    echo '<h1>Database Config</h1>';
-    print_r(Config::get('database.connections.mysql'));
-
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    } 
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-    }
-
-    echo '</pre>';
-
-});
-
-
-Route::get('/mysql-connection-test', function() {
-
-
-	$results = DB::select('SHOW DATABASES;');
-
-
-	return Pre::render($results, 'Results');
-
-
-});
 
 /******************************************/
 
@@ -283,90 +209,19 @@ Route::get('/practice-delete', function() {
 
 });
 
-/**********User Authentication********************/
+
+/*-------------------------------------------------------------------------------------------------
+// ! Debug
+Implicit
+-------------------------------------------------------------------------------------------------*/
+# Implicit routing
+Route::controller('debug', 'DebugController');
 
 
-// app/routes.php`:
-
-Route::get('/signup',
-    array(
-        'before' => 'guest',
-        function() {
-            return View::make('signup');
-        }
-    )
-);
-
-
-Route::post('/signup', 
-    array(
-        'before' => 'csrf', 
-        function() {
-
-            $user = new User;
-            $user->email    = Input::get('email');
-            $user->password = Hash::make(Input::get('password'));
-            $user->email    = Input::get('email');
-
-            # Try to add the user 
-            try {
-                $user->save();
-            }
-            # Fail
-            catch (Exception $e) {
-                return Redirect::to('/signup')->with('flash_message', 'Sign up failed; please try again.')->withInput();
-            }
-
-            # Log the user in
-            Auth::login($user);
-
-            return Redirect::to('/list')->with('flash_message', 'Welcome to Foobooks!');
-
-        }
-    )
-);
-
-
-Route::get('/login',
-    array(
-        'before' => 'guest',
-        function() {
-            return View::make('login');
-        }
-    )
-);
-
-
-Route::post('/login', 
-    array(
-        'before' => 'csrf', 
-        function() {
-
-            $credentials = Input::only('email', 'password');
-
-            if (Auth::attempt($credentials, $remember = true)) {
-                return Redirect::intended('/')->with('flash_message', 'Welcome Back!');
-            }
-            else {
-                return Redirect::to('/login')->with('flash_message', 'Log in failed; please try again.');
-            }
-
-            return Redirect::to('login');
-        }
-    )
-);
-
-
-# /app/routes.php
-Route::get('/logout', function() {
-
-    # Log out
-    Auth::logout();
-
-    # Send them to the homepage
-    return Redirect::to('/');
-
-});
-
-
+/*
+# Explicit routing
+Route::get('/debug/', 'DebugController@index');
+Route::get('/debug/trigger-error', 'Debug Controller@triggerError');
+Route::get('/debug/routes', 'DebugController@routes');
+*/
 
